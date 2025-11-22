54b47ef3c6101c9cb92f16579fac31b3cfd23f9a
leedonggyu <zkfmapf123@naver.com>
2025-11-22 19:46:51 +0900

    wip-0f5aec3b

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(ctx context.Context, request events.ALBTargetGroupRequest) (events.ALBTargetGroupResponse, error) {
+
+	fmt.Println(request)
+
 	return events.ALBTargetGroupResponse{
 		StatusCode: 200,
 		Headers: map[string]string{
